b602e5f61c45238a7d42309903de45c8a8f57cb5
Pedro Torres <phesgot001@gmail.com>
2021-05-13 23:49:35 -0300

    Login

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -2,9 +2,10 @@ import 'dart:io';
 
 import 'package:chat/text_composer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -13,33 +14,122 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
 
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey<ScaffoldState>();
+
+  FirebaseUser _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.onAuthStateChanged.listen((user) {
+      _currentUser = user;
+    });
+  }
+
+  Future<FirebaseUser> _getUser() async {
+    if (_currentUser != null) return _currentUser;
+
+    try {
+      final GoogleSignInAccount googleSignInAccount =
+      await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken,
+      );
+
+      final AuthResult authResult =
+      await FirebaseAuth.instance.signInWithCredential(credential);
+
+      final FirebaseUser user = authResult.user;
+      return user;
+    } catch (error) {
+      return null;
+    }
+  }
+
   void _sendMessage({String text, File imgFile}) async {
+    final FirebaseUser user = await _getUser();
 
     Map<String, dynamic> data = {};
 
-    if(imgFile != null){
-      StorageUploadTask task = FirebaseStorage.instance.ref().child(
-        DateTime.now().millisecondsSinceEpoch.toString()
-      ).putFile(imgFile);
+    if (user != null) {
+      _scaffoldkey.currentState.showSnackBar(
+          SnackBar(
+            content: Text('Não foi possivel fazer o login. Tente novamente!'),
+            backgroundColor: Colors.red,
+          )
+      );
 
-      StorageTaskSnapshot taskSnapshot = await task.onComplete;
-      String url = await taskSnapshot.ref.getDownloadURL();
-      data['imgUrl'] = url;
+      Map<String, dynamic> data = {
+        "uid": user.uid,
+        "senderName": user.displayName,
+        "senderPhotoUrl": user.photoUrl,
+      };
+
+      if (imgFile != null) {
+        StorageUploadTask task = FirebaseStorage.instance
+            .ref()
+            .child(DateTime
+            .now()
+            .millisecondsSinceEpoch
+            .toString())
+            .putFile(imgFile);
+
+        StorageTaskSnapshot taskSnapshot = await task.onComplete;
+        String url = await taskSnapshot.ref.getDownloadURL();
+        data['imgUrl'] = url;
+      }
+
+      if (text != null) data['text'] = text;
+
+      Firestore.instance.collection('messages').add(data);
     }
-
-    if(text != null) data['text'] = text;
-
-    Firestore.instance.collection('messages').add(data);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Olá'),
-        elevation: 0,
-      ),
-      body: TextComposer(_sendMessage),
-    );
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        key: _scaffoldkey,
+        appBar: AppBar(
+          title: Text('Olá'),
+          elevation: 0,
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: Firestore.instance.collection('messages').snapshots(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    default:
+                      List<DocumentSnapshot> documents =
+                      snapshot.data.documents.reversed.toList();
+
+                      return ListView.builder(
+                          itemCount: documents.length,
+                          reverse: true,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              title: Text(documents[index].data['text']),
+                            );
+                          });
+                  }
+                },
+              ),
+            ),
+            TextComposer(_sendMessage),
+          ],
+        ),
+      );
+    }
   }
-}
+
